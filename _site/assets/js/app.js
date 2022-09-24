3ea8236122b68b8f2cde4710f4fd670339bbf940
Jan Milosh <janmilosh@gmail.com>
2022-09-24 14:49:35 -0400

    Basic working top nav

---
+++ _site/assets/js/app.js
@@ -0,0 +1,24 @@
+window.onload = (event) => {
+
+  const toggle = document.querySelector("header #toggle");
+  const menu = document.querySelector("header #menu");
+  const icon = document.querySelector("header #icon");
+  
+  function toggleMenu() {
+    if (menu.classList.contains("active")) {
+      menu.classList.remove("active");
+      icon.classList.remove("fa-times");
+      icon.classList.add("fa-bars");
+    } else {
+        menu.classList.add("active");
+        icon.classList.add("fa-times");
+        icon.classList.remove("fa-bars");
+    }
+  }
+  
+  toggle.addEventListener("click", toggleMenu);
+
+  let year = document.querySelector("#year");
+  year.textContent = (new Date()).getFullYear();
+
+};
